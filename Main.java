87a296fb44fba4c78007fd9d467dae724e46f295
D0tsy <85434905+D0tsy@users.noreply.github.com>
2024-05-28 03:35:17 +0800

    Tutor

--- Main.java
+++ Main.java
@@ -8,21 +8,4 @@ public class Main {
         System.out.println("test");
     }
 }
-/*
-public class Tutor{
-    public String name;
-    public String academicInfo;
-    public String specialization;
-    public double hourly_rate;
-    public list<String> reservations;
 
-
-    public Tutor(String name,String academicInfo,String specialization,double hourly_rate,){
-        this.name=name;
-        this.academicInfo=academicInfo;
-        this.specialization=specialization;
-        this.hourly_rate=hourly_rate;
-        this.reservations=new List<String>();
-    }
-}
-*/
--- Tutor.java
+++ Tutor.java
@@ -1,7 +1,7 @@
 public class Tutor {
-    private String[] TutorName = new String[10];
-    private float[] TutorRate = new float[10];
-    private String[] TutorAcademics = new String[10];
-    private String[] Specialization = new String[10];
-    private String[] Reservations = new String[10];
+    private String name = "Jeremy";
+    private float rate = 15.0;
+    private String academics = "Masters Degree in Computer Science";
+    private String Specialization = "Computer Science";
+    private String reservations = "1234567"
 }
