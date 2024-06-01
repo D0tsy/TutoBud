d8614d8e7c32b718d620b741c2e4d9ca62d1a722
D0tsy <85434905+D0tsy@users.noreply.github.com>
2024-06-01 22:35:27 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,39 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Student.studName();
         Student.phoneNum();
-
-        //Tutors test
-        int x=0;
-        do{
-            Scanner sc = new Scanner(System.in);
-            System.out.println("1. add tutors");
-            System.out.println("2. print tutors");
-            System.out.println("3. remove Tutor");
-            System.out.println("4. exit");
-
-            String choice = sc.nextLine();
-
-            switch(choice){
-                case "1":
-                    Tutors.add();
-                    break;
-
-                case "2":
-                    Tutors.print();
-                    break;
-
-                case "3":
-                    Tutors.remove();
-                    break;
-
-                case "4":
-                    x=1;
-                    break;
-
-                default:
-                    break;
-            }
-        }while (x!=1);
+        
     }
 }
 
