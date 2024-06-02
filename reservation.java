84292c078d0f6c8f071ea26430fc81c67c2f2352
D0tsy <85434905+D0tsy@users.noreply.github.com>
2024-06-02 20:20:30 +0800

    name

--- reservation.java
+++ reservation.java
@@ -1,13 +1,13 @@
 //reservation class
 //Nur Atiekah Izzatie 22005669
 
-public class Reservation {
+public class reservation {
     private String studentName;
     private String tutorName;
     private String date;
     private String time;
 
-    public Reservation(String studentName, String tutorName, String date, String time) {
+    public reservation(String studentName, String tutorName, String date, String time) {
         this.studentName = studentName;
         this.tutorName = tutorName;
         this.date = date;
