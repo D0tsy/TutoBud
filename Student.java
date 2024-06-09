f47a16581e40488582a2404cf983b60f6b7cd306
D0tsy <85434905+D0tsy@users.noreply.github.com>
2024-06-09 23:40:28 +0800

    minor spacing changes

--- Student.java
+++ Student.java
@@ -4,15 +4,16 @@ public class Student{
     private String name;
     private String email;
     private String phone;
+
     public Student(String name, String email, String phone){
         this.name = name;
         this.email = email;
         this.phone = phone;
     }
+    
     public String getStudentName() {
         return name;
     }
-
     public String getStudentEmail(){
         return email;
     }
@@ -23,5 +24,4 @@ public class Student{
         return "Student Name: "+name+", Stud" +
                 "ent Email: "+email+", Student Phone: "+phone;
     }
-
 }
