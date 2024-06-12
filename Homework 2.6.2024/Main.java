b67bd03a24457560274d242663b0f76721e3e6cc
D0tsy <85434905+D0tsy@users.noreply.github.com>
2024-06-12 23:11:53 +0800

    Create Main.java

---
+++ Homework 2.6.2024/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Student - Muhammad Asyraf Arsyad
+        Student sInfo = new Student();
+        sInfo.name();
+        sInfo.age();
+        sInfo.gender();
+        sInfo.academic();
+        sInfo.address();
+        sInfo.contact();
+
+        //Students - Wan Muhammad Adib Shahmi
+        Students students = new Students();
+        students.addstudent();
+        students.removestudent();
+        students.printstudent();
+        students.findstudent();
+
+        //Tutor = Riz Haikal Bin Jasri]
+        Tutor tInfo = new Tutor();
+        tInfo.name();
+        tInfo.rate();
+        tInfo.academics();
+        tInfo.specialization();
+        tInfo.reservations();
+
+        //Tutors = Riz Haikal Bin Jasri
+        Tutors tutors = new Tutors();
+        tutors.add();
+        tutors.remove();
+        tutors.print();
+
+    }
+}
