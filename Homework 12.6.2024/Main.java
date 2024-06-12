64a92624c3e3e16b15a4f67f8b6d8d3482eb1407
D0tsy <85434905+D0tsy@users.noreply.github.com>
2024-06-12 23:05:23 +0800

    Update Main.java

--- Homework 12.6.2024/Main.java
+++ Homework 12.6.2024/Main.java
@@ -1,1 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Students - Wan Muhammad Adib Shahmi
+        Students students = new Students();
+        students.addstudent("abu");
+        students.addstudent("ali");
+        students.removestudent("amin");
+        students.printstudent();
+        students.findstudent("abu");
+
+        Tutor Adam = new Tutor("+601236547" , "Adam@gmail.com" , "1001", "Object Oriented Programming");
+
+        //Tutors = Riz Haikal Bin Jasri
+        Tutors tutors = new Tutors();
+
+        tutors.add(Adam);
+        tutors.print();
+        tutors.remove(Adam);
+        tutors.print();
+    }
+}
 
