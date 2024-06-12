4e2e92c3abe04c1ae9984dec890511ff7ff30ec7
D0tsy <85434905+D0tsy@users.noreply.github.com>
2024-06-12 23:12:55 +0800

    Create Students.java

---
+++ Homework 2.6.2024/Students.java
@@ -0,0 +1,28 @@
+//Students class
+// Wan Muhammad Adib Shahmi  24000495 (dummy class)
+import java.util.ArrayList;
+
+public class Students {
+    private ArrayList<String> studentlist;
+
+    public Students() {
+        studentlist = new ArrayList<> ();
+    }
+
+    public void addstudent(){
+        System.out.println("addstudent");
+    }
+
+    public void removestudent(){
+        System.out.println("removestudent");
+    }
+
+    public void printstudent(){
+        System.out.println("printstudent");
+    }
+
+    public void findstudent(){
+        System.out.println("findstudent");
+    }
+
+}
